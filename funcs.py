8319fa425ebae7d27dd88447540b7bce506df937
Egor Garin <garinegor@gmail.com>
2018-01-03 23:35:33 +0300

    first commit

---
+++ funcs.py
@@ -0,0 +1,28 @@
+import cognitive_face as CF
+import os
+
+KEY = '3c064f7a007c4ce2834f49aa740bcc42'
+CF.Key.set(KEY)
+CF.BaseUrl.set('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/')
+
+def who_is(path,group_id):
+	to_r=None
+	faces_result = CF.face.detect(path)
+	if len(faces_result)==1:
+		result=CF.face.identify([faces_result[0]['faceId']],group_id)
+		print(result)
+		if len(result[0]['candidates'])!=0:
+			result=result[0]['candidates'][0]
+			print(result)
+			if result['confidence']>0.6:
+				to_r=CF.person.get(group_id,result['personId'])['name']
+	return to_r
+def add_person(name,group=1):
+	personId=CF.person.create(group,name)['personId']
+	for filename in os.listdir('./persons/'+name):
+		if filename[-4:]==".jpg":
+			print('./persons/'+name+'/'+filename)
+			CF.person.add_face('./persons/'+name+'/'+filename,group,personId)
+		else:
+			print(filename)
+	CF.person_group.train(group)
---
+++ main.py
@@ -0,0 +1,32 @@
+from picamera import PiCamera
+from time import sleep
+from funcs import *
+import RPi.GPIO as GPIO
+import pygame, os
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(13, GPIO.OUT)
+
+camera = PiCamera()
+camera.vflip = True
+camera.hflip = True
+
+while True:
+    if GPIO.input(21) == False:
+        path='./guests/new.jpg'
+        camera.capture(path)
+        camera.start_preview()
+        sleep(4)
+        camera.stop_preview()
+        name=who_is(path)
+        if name!=None:
+            pygame.mixer.Sound('./voice/'+name+'.wav').play()
+
+        sleep(0.2)
+
+camera.start_preview()
+camera.stop_preview()
+camera.capture('./persons/')
